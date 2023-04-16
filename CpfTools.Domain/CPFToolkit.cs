79a277eb1b6595aa9d08a7d2aa4d483fae5e516f
Berit Carvalho <beritcarvalho@gmail.com>
2023-04-16 13:27:58 -0300

    - Adicionado ferramentas para adcionar ou remover mascara do cpf
    - Adicionado sumário dos métodos

--- CpfTools.Domain/CPFToolkit.cs
+++ CpfTools.Domain/CPFToolkit.cs
@@ -4,10 +4,38 @@ namespace CpfTools.Domain
 {
     public static class CPFToolkit
     {
+        private static readonly Validator _validator = new Validator();
+        private static readonly MaskCpf _maskUtis = new MaskCpf();
+
+        /// <summary>
+        /// Checks if the entered value is a valid CPFRemove CPF mask.
+        /// </summary>
+        /// <param name="cpf">Value to be validated</param>
+        /// <param name="replace">Optional parameter, which activates the removal of any non-numeric character.To activate, parameterize the value true.</param>
+        /// <returns>Returns true if it is a valid cpf and false if it is invalid.</returns>
         public static bool Validate(string cpfNumber)
         {
-            Validator validator = new();
-            return validator.Validate(cpfNumber);
+            return _validator.Validate(cpfNumber);
+        }
+
+        /// <summary>
+        /// Transforms the CPF number into a CPF with a default mask.
+        /// </summary>
+        /// <param name="cpf">The CPF to be validated must contain only numbers and have 11 digits.</param>
+        /// <returns>Transforms the CPF number into a CPF with a default mask.</returns>
+        public static string Mask(string cpfNumber)
+        {          
+            return _maskUtis.Mask(cpfNumber);
+        }
+
+        /// <summary>
+        /// Remove CPF mask.
+        /// </summary>
+        /// <param name="cpf">The parameter entered has no restriction on quantity or values, but must contain 11 numeric digits</param>
+        /// <returns>Return a string of 11 numeric digits</returns>
+        public static string RemoveMask(string cpfNumber)
+        {
+            return _maskUtis.RemoveCPFMask(cpfNumber);
         }
     }
 }
---
+++ CpfTools.Domain/Utilities/MaskCpf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CpfTools.Domain.Utilities
+{
+    public class MaskCpf
+    {
+        public string Mask(string cpf)
+        {
+            if (cpf.Length != 11)
+                throw new ArgumentException("CPF deve ter exatamente 11 caracteres.", nameof(cpf)); ;            
+            
+            if (!Regex.Match(cpf, "^[0-9]+$").Success) throw new ArgumentException("O parâmetro deve ter apenas números.", nameof(cpf));
+
+            string maskedCpf = Regex.Replace(cpf, @"(\d{3})(\d{3})(\d{3})(\d{2})", "$1.$2.$3-$4");
+            return maskedCpf; 
+
+        }
+        
+        public string RemoveCPFMask(string cpf)
+        {
+            cpf = Regex.Replace(cpf, "[^0-9]", "");
+
+            if(cpf.Length != 11) throw new ArgumentException("O valor informado para o CPF é inválido. Certifique-se de que foram informados 11 caracteres numéricos.");
+
+            return cpf;
+        }
+
+    }
+}
